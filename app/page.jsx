644668db3210c8a1625c15480733a06d92a71abc
git-sharwaree <sharwaree.nuhpw22@sinhgad.edu>
2025-08-12 00:03:59 +0530

    chxnges

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,10 @@
 
 import { Button } from "@/components/ui/button";
 import HeroSection from "@/components/hero";
+import { Card, CardContent } from "@/components/ui/card";
+import { features } from "@/data/features";
+
+
  
 export default function Home() {
   return ( 
@@ -8,11 +12,16 @@ export default function Home() {
     <div className="grid-background"></div>
     <HeroSection/>
 
-    <section>
-      <div>
-        <h2>Features for Your Career Growth</h2>
+    <section className = "w-full py-12 md:py-24 lg:py-32 bg-background">
+      <div className="container mx-auto px-4 md:px-6">
+        
+        <div className= "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6x1 mx-auto">
+
+        </div>
         </div>
         </section>
+
+            
   </div>
   );
 
