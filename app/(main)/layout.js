9baf496d4ddf4755abe97f656ef9b78355ddcd95
git-sharwaree <sharwaree.nuhpw22@sinhgad.edu>
2025-09-01 23:21:18 +0530

    .

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
 const MainLayout = () => {
+// redirect user after onboard    
+    return (
+        <div className= "container mx-auto mt-24 mb-20">{children}</div>
+    )
     
 }
